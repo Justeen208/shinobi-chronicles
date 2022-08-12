8162a9e41a368b23000acb9ab24b5f53c912c09e
Levi Meahan <lsmjudoka@gmail.com>
2022-08-12 01:07:59 -0700

    Fix collision point determination and add tests

--- classes/API.php
+++ classes/API.php
@@ -6,15 +6,16 @@ require __DIR__ . '/APIResponse.php';
 
 class API {
     #[NoReturn]
-    public static function exitWithError($message) {
+    public static function exitWithError(string $message, array $debug_messages = []): void {
         echo json_encode([
-            'errors' => $message
+            'errors' => $message,
+            'debug' => $debug_messages
         ]);
         exit;
     }
 
     #[NoReturn]
-    public static function exitWithData(array $data, array $errors, array $debug_messages) {
+    public static function exitWithData(array $data, array $errors, array $debug_messages): void {
         echo json_encode([
             'data' => $data,
             'debug' => $debug_messages,
